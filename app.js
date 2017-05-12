5152a34a6ad6f4d8b8655cfa12a0c13d2437e2c4
Andreza Feu <andrezafeu@gmail.com>
2017-05-11 23:02:25 -0400

    Prompt user for input

--- app.js
+++ app.js
@@ -13,9 +13,24 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+  function (session) {
+    builder.Prompts.text(session, 'Hi! What is your name?');
+  },
+  function (session, results) {
+    session.send('Hello %s!', results.response);
+  }
+]);
